f11cd4b0801c895d4d8352c7224839e90741d239
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-01-02 20:28:29 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,10 +18,10 @@ know = {
             function talk() {
                 var user = document.getElementById("userBox").value;
                 document.getElementById("userBox").value = "";
-                document.getElementById("chatLog").innerHTML += user+"<br>";
+                document.getElementById("chatLog").innerHTML += user+"-<br>";
                 if (user in know) {
                     document.getElementById("chatLog").innerHTML += know[user]+"<br>";
                 } else {
-                    document.getElementById("chatLog").innerHTML += "I don't understand...<br>";
+                    document.getElementById("chatLog").innerHTML += "-I don't understand...<br>";
                 }
             } 
