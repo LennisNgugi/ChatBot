f1cd90182693c2fedd6515397e508326199ce70b
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-01-02 20:40:47 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,8 +20,8 @@ know = {
                 document.getElementById("userBox").value = "";
                 document.getElementById("chatLog").innerHTML += user+"<br>";
                 if (user in know) {
-                    document.getElementById("chatLog").innerHTML += know[user]+"-<br>";
+                    document.getElementById("chatLog").innerHTML += - know[user]+"<br>";
                 } else {
-                    document.getElementById("chatLog").innerHTML += "-I don't understand...<br>";
+                    document.getElementById("chatLog").innerHTML += "- I don't understand...<br>";
                 }
             } 
