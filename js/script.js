4437794c3845f8b6f30d8d647b9835bd2dfcdbb6
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-01-03 16:52:46 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 know = {
-                "How old are you?" : "- infinity",        
-                "How old are you" : "- infinity",	             
-                "How old are you" : "- infinity",
-                "how old are you" : "- infinity",	               
-                "how old are you" : "- infinity",
-                "Know any cool people" : "There's this girl called Orla who seems pretty chill :D"	                
-                "Know any cool people" : "There's this girl called Orla who seems pretty chill :D",
-                "What's your favourite song?" : "- Nights by Frank",	                
-                "What's your favourite song?" : "- Nights by Frank",
-                "how are you?" : "- good",	                
-                "how are you?" : "- good",
-                "it's been fun" : "- God Speed",	                
-                "it's been fun" : "- God Speed"       
+              "hello" : "- Hi"     
 };
             function talk() {
                 var user = document.getElementById("userBox").value;
