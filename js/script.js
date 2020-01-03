2f660cb2f585add26c0be071c322545815147d64
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-01-03 17:31:24 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,8 @@ know = {
               "Hi" : "-Hello :D",
               "What's your name?" : "- My name is Starbot",
               "How old are you?" : "- infinity",
-              "Any advice?" : "- Nah, Good Speed through",              
-              "What's your favourite song?" : "- Nights by Frank",  
-              "What sort of things do you enjoy doing?" : "¯\_(ツ)_/¯",              
-              "Know any cool people" : "-There's this girl called Orla who seems pretty chill :D"
+              "Any advice?" : "- Nah, Good Speed through"              
+              
 };
             function talk() {
                 var user = document.getElementById("userBox").value;
